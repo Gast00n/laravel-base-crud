624c16039fddade27819bd944dbbc15623a2937b
Marco Giulietti <ga6t00n@gmail.com>
2021-01-21 17:35:11 +0100

    Start working on Crud!

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <h1>List of all Classes</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia nostrum mollitia, exercitationem inventore illo est ipsum repudiandae unde facere quis, earum molestias eum, tempora impedit excepturi ea totam placeat. Nemo!</p>
+    </div>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <p class="text-center">
+        School site powered by Class19!
+    </p>
+</footer>
